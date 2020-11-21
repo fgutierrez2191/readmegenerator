ae51956e09d67fd2f5a1157d3fec6a07d52270df
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-11-21 16:53:45 -0500

    generate markdown page finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # Title
+  ${data.title}
 
+  ## Username
+  ${data.username}
+
+  ## Description
+  ${data.description}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Technologies
+  ${data.technologies}
+
+  ## License
+  ${data.license}
+
+  ## *For Questions*
+  contact:
+
+  Github:[${data.Username}](https://github.com/fgutierrez2191)
 `;
 }
 
