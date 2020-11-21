8845babe2c95ded5d6e0e9b99cb90082f4c63f4a
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-11-21 16:08:55 -0500

    added question prompts

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
 
+// array of questions for user
+const questions =  [
+    {
+        type: "input",
+        message: "What is your Github Username?",
+        name: "username"
+    }
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What would you like to put in the description of your README?",
+        name: "description"   
+    },
+     {
+        type: "input",
+        message: "Please list the project contributors.",
+        name: "contributors"   
+    },
+    {
+        type: "checkbox",
+        message: "Please check the technologies that you used.",
+        choices: ["HTML", "CSS", "JAVASCRIPT", "JQUERY", "NODE.JS", "REACT", "BOOTSTRAP"]
+    },
+    {
+        type: "input",
+        message: "Please enter the license that you would like to use",
+        name: " license"
+    },
 ];
 
 // function to write README file
